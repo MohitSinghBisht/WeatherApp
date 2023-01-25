d44795270fbbe5aaa5863e7b327bbac68a40c4b0
Mohit Bisht <mohit.14061@gnindia.dronacharya.info>
2023-01-25 19:48:01 +0530

    weather

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Weather } from './weather.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'weather-app';
+export class AppComponent implements OnInit{
+  cityName='New Delhi';
+  weather:Weather=new Weather();
+  constructor(private http:HttpClient){
+  }
+
+  ngOnInit() {
+    let params = new HttpParams().set("city",this.cityName);
+    let header = new HttpHeaders()
+    .set('X-RapidAPI-Key', 'e8703d9480mshc90f5237d67e230p102437jsn9d6f87811b9b')
+    .set('X-RapidAPI-Host', 'weather-by-api-ninjas.p.rapidapi.com');
+  
+    const options = { params: params, headers: header };
+  
+    this.http.get('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather',options).subscribe((data:any)=>{
+      console.log(data);
+      this.weather=data;
+    })
+  }
+
+  // const options = {
+  //   method: 'GET',
+  //   headers: {
+  //     'X-RapidAPI-Key': 'e8703d9480mshc90f5237d67e230p102437jsn9d6f87811b9b',
+  //     'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+  //   }
+  // };
+  
+  onSubmit(){
+
+  let params = new HttpParams().set("city",this.cityName);
+  let header = new HttpHeaders()
+  .set('X-RapidAPI-Key', 'e8703d9480mshc90f5237d67e230p102437jsn9d6f87811b9b')
+  .set('X-RapidAPI-Host', 'weather-by-api-ninjas.p.rapidapi.com');
+
+  const options = { params: params, headers: header };
+
+  this.http.get('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather',options).subscribe((data:any)=>{
+    console.log(data);
+    this.weather=data;
+  })
+
+
+}
+
 }
---
+++ src/app/weather.model.ts
@@ -0,0 +1,24 @@
+export class Weather {
+    cloud_pct:number;
+    feels_like:number;
+    humidity:number;
+    max_temp:number;
+    min_temp:number;
+    sunrise:number;
+    sunset:number;
+    temp:number;
+    wind_degrees:number;
+    wind_speed:number;
+    
+}
+
+// cloud_pct:75
+// feels_like:16
+// humidity:88
+// max_temp:16
+// min_temp:16
+// sunrise:1674610963
+// sunset:1674649417
+// temp:16
+// wind_degrees:280
+// wind_speed:3.09
